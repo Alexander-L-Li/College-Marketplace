32959cd9d26b02d6d129ca2adde0986ad5da0cb3
Alexander Li <alxli@mit.edu>
2025-06-15 18:32:32 -0400

    db reqs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,6 @@ function App() {
     <Router>
       <div className="min-h-screen flex flex-col">
         <Header />
-
         <main className="flex-grow p-4">
           <Routes>
             <Route path="/" element={<Home />} />
