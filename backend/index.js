a4f5c5c759f58f8ea713d581c7738ebfbf404a10
Alexander Li <alxli@mit.edu>
2025-06-16 23:15:35 -0400

    connected frontend login to backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const app = express();
 const pool = require("./db");
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 require("dotenv").config();
+
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -7,31 +7,11 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow p-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile/:userId" element={<Profile />} />
-            <Route path="/item/:itemId" element={<ItemDetails />} />
-            <Route path="/sell" element={<SellForm />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-    // <div className="min-h-screen bg-gray-100">
-    //   {/* Scroll Bar */}
-    //   <header className="hidden">
-    //     <input
-    //       type="text"
-    //       placeholder="Search..."
-    //       className="sm:hidden w-full max-w-fit bg-white border border-gray-900 rounded-md shadow-sm"
-    //     />
-    //   </header>
-    //   <main className="p-8">{/* Your content here */}</main>
-    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
